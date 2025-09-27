0e27eefa024aadd7a466add58893af4687b9e0fc
tarrut <91079521+tarrut@users.noreply.github.com>
2025-09-27 21:11:06 +0200

    feat: created fetching of el pais
    introduced the logic to read the news from the rss feed of el pais. it reads the whole feed, it filters the news by date (today) and category (according to an interesting categories)

--- src/main.py
+++ src/main.py
@@ -1,17 +1,11 @@
 from dotenv import load_dotenv
 from mail import send_mail
+from news.elpais import fetch_elpais
 
 load_dotenv()
 
 message = {
     "subject": "Daily News Recap",
-    "message": """
-# Hello
-
-This is a **Markdown** email.
-
-- Item 1
-- Item 2
-"""
+    "message": fetch_elpais()
 }
 send_mail(message=message)
