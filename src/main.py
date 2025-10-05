53b192780a5e4a0ddc64c744a4a0089de39c3a6b
tarrut <91079521+tarrut@users.noreply.github.com>
2025-10-05 16:51:36 +0200

    feat: include news from diari ara

--- src/main.py
+++ src/main.py
@@ -2,15 +2,17 @@ from dotenv import load_dotenv
 import datetime as dt
 
 from mail import send_mail
+from agent import get_mail_message
 from news.elpais import fetch_elpais
 from news.diarisabadell import fetch_diari_sabadell
-from agent import get_mail_message
+from news.ara import fetch_ara
 
 load_dotenv()
 
 news = []
 news.append(fetch_elpais())
 news.append(fetch_diari_sabadell())
+news.append(fetch_ara())
 
 today = dt.datetime.now()
 
---
+++ src/news/ara.py
@@ -0,0 +1,17 @@
+import feedparser
+
+from utils import is_today, filter_entry
+
+URL = "https://www.ara.cat/rss/"
+
+def fetch_ara():
+    feed = feedparser.parse(URL)
+    interesting_entries = []
+
+    id = 1
+    for e in feed.entries:
+        if is_today(e):
+            interesting_entries.append(filter_entry(e, id))
+            id += 1
+    
+    return interesting_entries
