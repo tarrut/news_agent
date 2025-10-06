7b61b09e791cac0c0fa1b69953bc86d9faaa18b9
tarrut <91079521+tarrut@users.noreply.github.com>
2025-10-06 21:23:54 +0200

    fix: make date detection flexible

--- src/news/utils.py
+++ src/news/utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone, timedelta
+from dateutil import parser
 
 def is_interesting(entry, interesting_categories):
     categories = [t['term'] for t in entry.get('tags', [])]
@@ -6,16 +7,21 @@ def is_interesting(entry, interesting_categories):
 
 
 def is_today(entry):
-    entry_time = datetime.strptime(entry["published"], "%a, %d %b %Y %H:%M:%S %Z")
-    entry_time = entry_time.replace(tzinfo=timezone.utc)
+    entry_time = parser.parse(entry["published"])
+
+    if entry_time.tzinfo is None:
+        entry_time = entry_time.replace(tzinfo=timezone.utc)
+    else:
+        entry_time = entry_time.astimezone(timezone.utc)
+    
     now = datetime.now(timezone.utc)
     return (now - entry_time) <= timedelta(hours=24)
 
 
-def filter_entry(entry, id):
-    return {
-        "id": id,
-        "link": entry["link"],
-        "title": entry["title"],
-        "summary": entry["summary"],
-    }
+def filter_entry(entry, id_):
+    new = {"id": id_}
+    for key in ("link", "title", "summary"):
+        value = entry.get(key)
+        if value:
+            new[key] = value
+    return new
