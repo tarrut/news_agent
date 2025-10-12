2a71d1bd3b32c2af5c7bd304ff9a76ae2c101d89
tarrut <91079521+tarrut@users.noreply.github.com>
2025-10-12 10:45:35 +0200

    feat: add get_titulars to NewsFetcher

--- src/news_fetcher.py
+++ src/news_fetcher.py
@@ -13,10 +13,24 @@ class NewsFetcher():
     def __init__(self):
         self.news = {}
         self.fetch_news()
+        self.titulars = {}
 
     def fetch_news(self):
         for newspaper, fetch_function in URLS.items():
             self.news[newspaper] = fetch_function()
 
     def get_news(self):
+        if not self.news:
+            self.fetch_news()
         return self.news
+    
+    def get_titulars(self):
+        if not self.news:
+            self.fetch_news()
+
+        if not self.titulars:
+            for id, new in self.news.items():
+                self.titulars[id] = new["titular"]
+        
+        return self.titulars
+            
