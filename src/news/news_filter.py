810a78b03ce7cd66378e5bc656667eaeb08b44cb
tarrut <91079521+tarrut@users.noreply.github.com>
2025-10-18 19:52:42 +0200

    feat: add NewsFilter class

---
+++ src/news/news_filter.py
@@ -0,0 +1,36 @@
+
+from news.utils import is_today
+
+class NewsFilter:
+    """Class that filters the today news and improves their format
+    """
+
+    def __init__(self, raw_news: list[dict]):
+        """
+        Args:
+            raw_news: list of unprocessed raw news
+        """
+        self._raw_news = raw_news
+        self.news = {}
+
+    
+    def filter_news(self):
+        self.filter_news_by_date()
+        self.fix_news_format()
+
+
+    def filter_news_by_date(self):
+        self._raw_news = [new for new in self.raw_raw_news_news if is_today(new)]
+
+    
+    def fix_news_format(self):
+        id = 0
+        for new in self._raw_news:
+            self.news[id] = {
+                "title": new["title"],
+                "link": new["link"],
+                "publicated": new["pubDate"],
+                "description": new["description"],
+                "newspaper": new["newspaper"]
+            }
+            id += 1
