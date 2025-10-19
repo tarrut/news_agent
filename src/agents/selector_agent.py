233c1a0798a136e704e48c0607c2d37f6b418bfd
tarrut <91079521+tarrut@users.noreply.github.com>
2025-10-19 16:12:35 +0200

    feat: initial selector agent structure

---
+++ src/agents/selector_agent.py
@@ -0,0 +1,6 @@
+
+class SelectorAgent:
+
+    def __init__(self, news: dict[dict]):
+        self.news = news
+    
--- src/main.py
+++ src/main.py
@@ -5,22 +5,28 @@ from mail import send_mail
 from agent import get_mail_message
 from news.news_fetcher import NewsFetcher
 from news.news_filter import NewsFilter
+from agents.selector_agent import SelectorAgent
 
 load_dotenv()
 
+# Extract news from RSS feeds
 news_fetcher = NewsFetcher()
 news_fetcher.fetch()
 raw_news = news_fetcher.get_news()
 
+# Filter the raw news
 news_filter = NewsFilter(raw_news)
 news_filter.filter_news()
 news = news_filter.get_news()
 
-today = dt.datetime.now()
+# Select the most relevant news
+selector_agent = SelectorAgent(news)
 
-message = {
-    "subject": f"News Recap - {today.strftime("%A %x")}",
-    "message": get_mail_message(news)
-}
+# today = dt.datetime.now()
 
-send_mail(message=message)
+# message = {
+#     "subject": f"News Recap - {today.strftime("%A %x")}",
+#     "message": get_mail_message(news)
+# }
+
+# send_mail(message=message)
